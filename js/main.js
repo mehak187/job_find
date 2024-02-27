db9eed6abb5c45a691949eeaf19d0ebcc4bab4ac
shakaib-fabtechsol <shakaib@fabtechsol.com>
2024-02-27 16:42:37 +0500

    done

--- js/main.js
+++ js/main.js
@@ -1,80 +1,80 @@
 // ------js file-------
 
-$(document).ready(function(){
-    $("#toggleButton").click(function(){
-      $("#content").toggle();
-    });
-    $(".open-filter").click(function(){
-      $(".filter-menu").toggle();
-    });
+$(document).ready(function () {
+  $("#toggleButton").click(function () {
+    $("#content").toggle();
   });
-  $('.center2').slick({
-    dots: true,
-    arrows: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-                arrows: false,
-                slidesToShow: 2,
-                slidesToScroll: 2,
-            }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-              arrows: false,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-          }
-      }
-    ]
+  $(".open-filter").click(function () {
+    $(".filter-menu").toggle();
   });
-  $('.center').slick({
-    dots: true,
-    arrows: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 998,
-            settings: {
-                arrows: false,
-                slidesToShow: 2,
-                slidesToScroll: 2,
-            }
-        },
-        {
-          breakpoint: 778,
-          settings: {
-              arrows: false,
-              slidesToShow: 2,
-              slidesToScroll: 2,
-          }
+});
+$(".center2").slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
       },
-        {
-            breakpoint: 577,
-            settings: {
-                arrows: false,
-                slidesToShow: 1,
-              slidesToScroll: 1,
-            }
-        }
-    ]
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(".center").slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 998,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 778,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 577,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
-$(document).ready(function(){
-  $(".chats").click(function(){
+$(document).ready(function () {
+  $(".chats").click(function () {
     $(".chats").hide();
     $(".chats-msg").show();
   });
-  $(".chat-close").click(function(){
+  $(".chat-close").click(function () {
     $(".chats-msg").hide();
     $(".chats").show();
   });
